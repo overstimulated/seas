b651edb89a1657a34d58d42cf19b53bf9789fbbd
J-me Solomon <jme2go@yahoo.com>
2013-09-25 12:36:40 -0400

    make index.php redirect to the yii application

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
-
+//echo 'Hello world from Cloud9!';
+header('HTTP/1.1 307 Temporary Redirect');
+header('Location: sampleApp/');
 ?>
+<html>
+  <head>
+    <title>Moved</title>
+  </head>
+  <body>
+    <h1>Moved</h1>
+    <p>This page has moved to <a href="FileManager/">FileManager</a>.</p>
+  </body>
+</html>
